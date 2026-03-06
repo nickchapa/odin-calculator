e3e92ca81acfbbc0d4c01e1a9926e24cb85ad91a
Nick Chapa <nicky.chapa@hotmail.com>
2026-03-06 09:07:29 -0600

    move num and operator vars to top for scope

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 const display = document.getElementById('display');
 
 const digit_0 = document.getElementById('0');
@@ -6,10 +10,6 @@ digit_0.addEventListener('click', (e) => {
 
 const digit_1 = document.getElementById('1');
 digit_1.addEventListener('click', (e) => console.log('1 button clicked!'));
-
-let num1;
-let num2;
-let operator;
 
 function add(a, b){
     return a + b;
